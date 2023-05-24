d1b42acd87356c5f991885b51b63d00774460b79
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2023-05-24 10:52:26 -0400

    user log in/create

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,12 @@
 class Api::UsersController < ApplicationController
 
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json @user, status: :created, location: @book
-
+    user = User.new(user_params)
+    if user.save
+      render json: {user: UserSerializer.new(user), token: encode_token(user.id)} 
     else
-      render json @user.errors, status: :unprocessable_entry
+      render json:{errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
     end
-
   end
 
   private
