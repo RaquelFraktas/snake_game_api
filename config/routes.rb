14fbe4905f81fd469c0610526e57da725d096445
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2023-06-05 14:49:32 -0400

    add logged in route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :update, :index]
 
   post "/login", to: "sessions#create"
+  get "/logged_in", to: "sessions#autologin"
   
 end
